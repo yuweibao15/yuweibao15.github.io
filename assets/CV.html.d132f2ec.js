ce9243a7cbb8ef347cb08e0abd031bfa7a62465a
yuweibao15 <89409924+yuweibao15@users.noreply.github.com>
2022-09-28 05:04:08 +0000

    Deploying to gh-pages from @ yuweibao15/yuweibao15.github.io@058138f8584cc3b78aef0d279c7f2e7071d847f7 🚀

--- assets/CV.html.d132f2ec.js
+++
@@ -1,4 +0,0 @@
-import{_ as t}from"./_plugin-vue_export-helper.cdc0426e.js";import{o,c as s,a as e,d as n,b as r,e as c,r as i}from"./app.82284749.js";const l={},m=r("h2",{id:"here-is-my-cv",tabindex:"-1"},[r("a",{class:"header-anchor",href:"#here-is-my-cv","aria-hidden":"true"},"#"),c(" Here is my CV:")],-1);function d(_,p){const a=i("PDF");return o(),s("div",null,[e(" Content before `more` comment is regarded as page excerpt. "),e(" more "),m,e(` Default PDF viewer:
-<PDF url="/sample.pdf" />
-PDF viewer with initial page 2:
-<PDF url="/sample.pdf" :page="2" /> `),e(" PDF viewer without toolbar: "),n(a,{url:"/assets/Yuwei_Bao_CV.pdf",toolbar:!1})])}const u=t(l,[["render",d],["__file","CV.html.vue"]]);export{u as default};
